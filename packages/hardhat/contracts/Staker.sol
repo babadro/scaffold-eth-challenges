eb8e519fde52d52bcb6e79455b530c14f4bdf72f
babadro <babadro@gmail.com>
2022-08-27 09:01:15 +0200

    stake function and event

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -5,15 +5,34 @@ import "hardhat/console.sol";
 import "./ExampleExternalContract.sol";
 
 contract Staker {
+    // External contract that will old stacked funds
+    ExampleExternalContract public exampleExternalContract;
 
-  ExampleExternalContract public exampleExternalContract;
+    // Balances of the user's stacked funds
+    mapping(address => uint256) public balances;
 
-  constructor(address exampleExternalContractAddress) {
-      exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
-  }
+    // Staking threshold
+    uint256 public constant threshold = 1 ether;
+
+    // Contract's Events
+    event Stake(address indexed sender, uint256 amount);
+
+    constructor(address exampleExternalContractAddress) public {
+        exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
+    }
+
+    function stake() public payable {
+        // update the user's balance
+        balances[msg.sender] += msg.value;
+
+        // emit the event to notify the blockchain that we have correctly Staked some fund for the user
+        emit Stake(msg.sender, msg.value);
+    }
 
   // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
   // ( Make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
+
+
 
 
   // After some `deadline` allow anyone to call an `execute()` function
