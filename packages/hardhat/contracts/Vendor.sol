d2834990076a7a4d9293aa6624bd7339d0a72151
babadro <babadro@gmail.com>
2022-09-16 07:51:25 +0200

    part 1 and 2: create a vendor contract. buy and send tokens

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -6,18 +6,49 @@ import "./YourToken.sol";
 
 contract Vendor is Ownable {
 
-  //event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+    // Our Token Contract
+    YourToken yourToken;
 
-  YourToken public yourToken;
+    // token price for ETH
+    uint256 public tokensPerEth = 100;
 
-  constructor(address tokenAddress) {
-    yourToken = YourToken(tokenAddress);
-  }
+    // Event that log buy operation
+    event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
 
-  // ToDo: create a payable buyTokens() function:
+    constructor(address tokenAddress) {
+        yourToken = YourToken(tokenAddress);
+    }
 
-  // ToDo: create a withdraw() function that lets the owner withdraw ETH
+    /**
+    * @notice Allow users to buy token for ETH
+  */
+    function buyTokens() public payable returns (uint256 tokenAmount) {
+        require(msg.value > 0, "Send ETH to buy some tokens");
 
-  // ToDo: create a sellTokens(uint256 _amount) function:
+        uint256 amountToBuy = msg.value * tokensPerEth;
 
+        // check if the Vendor Contract has enough amount of tokens for the transaction
+        uint256 vendorBalance = yourToken.balanceOf(address(this));
+        require(vendorBalance >= amountToBuy, "Vendor contract has not enough tokens in its balance");
+
+        // Transfer token to the msg.sender
+        (bool sent) = yourToken.transfer(msg.sender, amountToBuy);
+        require(sent, "Failed to transfer token to user");
+
+        // emit the event
+        emit BuyTokens(msg.sender, msg.value, amountToBuy);
+
+        return amountToBuy;
+    }
+
+    /**
+    * @notice Allow the owner of the contract to withdraw ETH
+  */
+    function withdraw() public onlyOwner {
+        uint256 ownerBalance = address(this).balance;
+        require(ownerBalance > 0, "Owner has not balance to withdraw");
+
+        (bool sent,) = msg.sender.call{value: address(this).balance}("");
+        require(sent, "Failed to send user balance back to the owner");
+    }
 }
